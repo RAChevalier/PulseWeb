423a34ec93a346d57246dbce2b554079b983d8ea
RAChevalier <leblanc.rac@gmail.com>
2014-09-16 13:20:46 +1000

    Reworked login
    Made some changes to database

--- getsession.php
+++ getsession.php
@@ -1,7 +1,7 @@
 <?PHP
 session_start();
 if(isset($_SESSION['user'])){
-	echo $_SESSION['user'];
+	echo $_SESSION['user'][0].",".$_SESSION['user'][1].",".$_SESSION['user'][2].",".$_SESSION['user'][3];
 } else {
 	echo "unauthorized";
 }
--- login.php
+++ login.php
@@ -7,13 +7,27 @@ if($_POST['user'] != ""){
 	$conn = mysql_connect('mysql.ict.swin.edu.au', 's4944623', '300193') or die ('Could not connect: '.mysql_error());
 	mysql_select_db('s4944623_db') or die('Could not select database');
 	
-	$result = mysql_query("select username from pulseadmin where username=$u and password=$p") or die (mysql_error());
+	$result = mysql_query("select username, firstname, lastname, level, status from pulseadmin where username='".$u."' and password='".$p."'", $conn) or die (mysql_error());
 
 	if(mysql_num_rows($result) == 0){
-		//header("Location: login.html?loginfailed");
+		header("Location: login.html?loginfailed");
 	} else {
-		$_SESSION['user'] = $u;
-		header('Location: home.html');
+		$row = mysql_fetch_assoc($result);
+		if($row['status'] == 0){
+			header("Location: login.html?unauthorized");
+		} else {
+			$user = $row['username'];
+			$first = $row['firstname'];
+			$last = $row['lastname'];
+			$level = $row['level'];
+			if($row['level'] == 0){
+				$_SESSION['user'] = array($user, $first, $last, $level);
+				header("Location: home.html");
+			} else {
+				$_SESSION['user'] = array($user, $first, $last, $level);
+				header('Location: agent.html');
+			}
+		}
 	}
 }
 ?>
