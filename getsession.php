a86b7ff08259f80d3867a9cbead281edcce4e683
RAChevalier <leblanc.rac@gmail.com>
2014-09-16 13:28:55 +1000

    Another rework for login

--- getsession.php
+++ getsession.php
@@ -1,6 +1,6 @@
 <?PHP
 session_start();
-if(isset($_SESSION['user'])){
+if(isset($_SESSION['user']) && $_SESSION['user'][3] == 0){
 	echo $_SESSION['user'][0].",".$_SESSION['user'][1].",".$_SESSION['user'][2].",".$_SESSION['user'][3];
 } else {
 	echo "unauthorized";
