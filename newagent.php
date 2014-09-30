74a4dc2f8221f0046a37dfceaa039169504a2a8f
Ryan Arsenna <leblanc.rac@gmail.com>
2014-09-30 22:24:15 +1000

    New Agent PHP

--- newagent.php
+++ newagent.php
@@ -1,10 +1,49 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<?PHP
+	include ('connect.php');
+	$fnameerr = $lnameerr = $doberr = $gendererr = $mobileerr = "";
+?>
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+<style>
+body{
+	font-family:'Open Sans';
+	background-color:#333;
+}
+table{
+	background-color:#111;
+	position:absolute;
+	margin:auto;
+	top:0;
+	bottom:0;
+	left:0;
+	right:0;
+}
+.label{
+	color:#36C;
+	text-align:right;
+}
+.err{
+	color:#C00;
+}
+</style>
 </head>
-
 <body>
+<div class='container'>
+<form>
+<table cellpadding="2px">
+<tr><td class='label'>First Name :</td><td><input type='text' name='fname'/></td></tr>
+<tr><td>&nbsp;</td><td class='err'><?PHP echo $fnameerr; ?></td></tr>
+<tr><td class='label'>Last Name :</td><td><input type='text' name='lname'/></td></tr>
+<tr><td></td><td class='err'><?PHP echo $lnameerr; ?></td></tr>
+<tr><td class='label'>Date of Birth :</td><td><input type='text' name='dob' placeholder="dd/mm/yyyy"/></td></tr>
+<tr><td></td><td class='err'><?PHP echo $doberr; ?></td></tr>
+<tr><td class='label'>Gender :</td><td><select name='gender'><option value=''></option><option value='M'>Male</option><option value='F'>Female</option></select></td></tr>
+<tr><td></td><td class='err'><?PHP echo $gendererr; ?></td></tr>
+<tr><td class='label'>Mobile :</td><td><input type='text' name='mobile'/></td></tr>
+<tr><td></td><td class='err'><?PHP echo $mobileerr; ?></td></tr>
+</table>
+</form>
+</div>
 </body>
 </html>
