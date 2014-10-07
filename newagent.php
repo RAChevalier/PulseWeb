027df3ec14c3be5f6d1638278c364d352d3679e9
Zwe Aung Myint Tun <zweaungmyintun@gmail.com>
2014-10-07 12:51:45 +1100

    not finish yet
    still have trouble in inserting

--- newagent.php
+++ newagent.php
@@ -1,6 +1,6 @@
 <?PHP
 	include ('connect.php');
-	$fnameerr = $lnameerr = $doberr = $gendererr = $mobileerr = "";
+	$fnameerr = $lnameerr = $doberr = $gendererr = $mobileerr = $emailerr = "";
 ?>
 <html>
 <head>
@@ -30,18 +30,25 @@ table{
 </head>
 <body>
 <div class='container'>
-<form>
+<form name='reg' method='post' action='reg_form.php'>
 <table cellpadding="2px">
-<tr><td class='label'>First Name :</td><td><input type='text' name='fname'/></td></tr>
-<tr><td>&nbsp;</td><td class='err'><?PHP echo $fnameerr; ?></td></tr>
-<tr><td class='label'>Last Name :</td><td><input type='text' name='lname'/></td></tr>
+<tr><td class='label'>Username :</td><td><input type='text' name='txt_username'/></td></tr>
+<tr><td class='label'>Password :</td><td><input type='password' name='txt_password'/></td></tr>
+<tr><td class='label'>First Name :</td><td><input type='text' name='txt_first_name'/></td></tr>
+<tr><td></td><td class='err'><?PHP echo $fnameerr; ?></td></tr>
+<tr><td class='label'>Last Name :</td><td><input type='text' name='txt_last_name'/></td></tr>
 <tr><td></td><td class='err'><?PHP echo $lnameerr; ?></td></tr>
-<tr><td class='label'>Date of Birth :</td><td><input type='text' name='dob' placeholder="dd/mm/yyyy"/></td></tr>
+<tr><td class='label'>Date of Birth :</td><td><input type='text' name='txt_dob' placeholder="dd/mm/yyyy"/></td></tr>
 <tr><td></td><td class='err'><?PHP echo $doberr; ?></td></tr>
 <tr><td class='label'>Gender :</td><td><select name='gender'><option value=''></option><option value='M'>Male</option><option value='F'>Female</option></select></td></tr>
 <tr><td></td><td class='err'><?PHP echo $gendererr; ?></td></tr>
-<tr><td class='label'>Mobile :</td><td><input type='text' name='mobile'/></td></tr>
+<tr><td class='label'>Mobile :</td><td><input type='text' name='txt_mobile' /></td></tr>
 <tr><td></td><td class='err'><?PHP echo $mobileerr; ?></td></tr>
+<tr><td class='label'>Email :</td><td><input type='text' name='txt_email'/></td></tr>
+<tr><td></td><td class='err'><?PHP echo $emailerr; ?></td></tr>
+<tr><td class='label'>Address :</td><td><input type='text' name='txt_address'/></td></tr>
+<tr><td></td><td class='err'><?PHP echo $mobileerr; ?></td></tr>
+<tr><td><input type='submit' name='btn_register' value='Register' /></td><td><input type='button' name='btn_clear' value='clear' /></td></tr>
 </table>
 </form>
 </div>
