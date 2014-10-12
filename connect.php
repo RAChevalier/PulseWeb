9b64383848cc940b642d7afacbaf664c92778111
Ryan Arsenna <leblanc.rac@gmail.com>
2014-10-12 21:12:21 +1100

    Fix some code to match byethost's server config

--- connect.php
+++ connect.php
@@ -1,10 +1,11 @@
 <?PHP
+	/*
 	$conn = mysql_connect('mysql.ict.swin.edu.au', 's4944623', '300193') or die ('Could not connect: '.mysql_error());
 	mysql_select_db('s4944623_db') or die('Could not select database');
-	
-	/*
-	Byethost does not allow connection...
-	$conn = mysql_connect('sql201.byethost5.com', 'b5_15320225', 'pulsepulse') or die ('Could not connect: '.mysql_error());
-	mysql_select_db('b5_15320225pulse') or die('Could not select database');
 	*/
+	
+	$conn = mysql_connect('sql201.byethost5.com', 'b5_15320225', 'pulsepulse') or die ('Could not connect: '.mysql_error());
+	mysql_select_db('b5_15320225_pulse') or die('Could not select database');
+	date_default_timezone_set("Australia/Melbourne");
+	$date = date('Y-m-d H:i:s');
 ?>
